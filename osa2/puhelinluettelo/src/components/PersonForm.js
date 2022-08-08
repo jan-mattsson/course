c919d809b772d97f59a660f962acfcf666560222
Jan Mattsson <39992000+jan-mattsson@users.noreply.github.com>
2022-08-08 22:09:47 +0300

    2.18 confirm and update number of existing

--- osa2/puhelinluettelo/src/components/PersonForm.js
+++ osa2/puhelinluettelo/src/components/PersonForm.js
@@ -12,22 +12,43 @@ const PersonForm = ({persons, setPersons}) => {
     
     const addPerson = (event) => {
         event.preventDefault()
+
+// if exists with different number number --> confirm & update
+// if exists with same number --> give error message??
+
+        const handleResponse = (response) => {
+          console.log("PUT response", response)
+          const index = persons.findIndex(p => p.id === response.data.id)
+          if ( index === -1){
+            console.log('Added', response.data)
+            setPersons(persons.concat(response.data))
+          }
+          else {
+            console.log('Replace', response.data)
+            const tmp = Array.from(persons)
+            tmp[index] = response.data
+            setPersons(tmp)
+          }
+          
+          event.target.name.value = ''  
+          event.target.phoneNumber.value = ''
+          setNewName('')
+          setNewNumber('')
+        }
+
         Validate(
           () => !persons.find(p => p.name === newName ),
           () => {
-
             const newPerson = {name: newName, key: newName, number: newNumber }
             PersonPersistence.AddNewPerson(newPerson)
-              .then(response => {
-                console.log("PUT response", response)
-                setPersons(persons.concat(response.data))
-                event.target.name.value = ''  
-                event.target.phoneNumber.value = ''
-                setNewName('')
-                setNewNumber('')
-            })            
+              .then(handleResponse)
           },
-          () => alert(`${newName} is already added to the phonebook`)
+          () => {            
+            const person = persons.find(p => p.name === newName)
+            if (window.confirm(`${person.name} is already in the phonebook, replace the old number with a new one?`))
+              PersonPersistence.UpdatePerson({...person, number: newNumber})
+                .then(handleResponse)
+          }
         )
     }
 
--- osa2/puhelinluettelo/src/components/PersonPersistence.js
+++ osa2/puhelinluettelo/src/components/PersonPersistence.js
@@ -7,7 +7,7 @@ const GetAllPersons = () => {
     .get(PERSONS_COLLECTION_URI)
 }
 
-const AddNewPerson = ({name, key, number }) => {
+const AddNewPerson = ({name, key, number}) => {
     return axios
     .post(PERSONS_COLLECTION_URI, {name:name, key:key, number:number})
 }
@@ -18,4 +18,10 @@ const DeletePerson =  (id) => {
       .delete(`${PERSONS_COLLECTION_URI}/${id}`)
 }
 
-export default {AddNewPerson, GetAllPersons, DeletePerson }
+const UpdatePerson = ({name, key, number, id}) => {
+  console.log('UPDATE', id)
+  return axios
+    .put(`${PERSONS_COLLECTION_URI}/${id}`, {name:name, key:key, number:number, id:id})
+}
+
+export default {AddNewPerson, GetAllPersons, DeletePerson, UpdatePerson }
